76dd61b564767a53ad90dc2364fde2af0e856f2c
Steven Miller <steven.jmiller@gmail.com>
2012-11-12 23:15:35 +1100

    Add link to clients - Added more clients in for testing on Litmus

--- grunt.js
+++ grunt.js
@@ -36,7 +36,8 @@ module.exports = function(grunt) {
             username : 'username',
             password : 'password',
             url : 'https://yoursite.litmus.com',
-            applications : ['gmail', 'hotmail']
+            //https://yoursite.litmus.com/emails/clients.xml
+            applications : ['gmailnew', 'hotmail', 'outlookcom', 'ol2000', 'ol2002', 'ol2003', 'ol2007', 'ol2010','ol2011', 'ol2013', 'appmail6','iphone3', 'iphone4', 'ipad3']
           },
           basepath : 'example/emails/'
         },
